4d9dc38170a91835b552902891934a7019d4ba05
Anjai Kumar <anjaikr@gmail.com>
2019-03-16 12:53:15 +0530

    Supermarket.API

--- Supermarket.API/Startup.cs
+++ Supermarket.API/Startup.cs
@@ -51,8 +51,11 @@ namespace Supermarket.API
             services.AddAutoMapper();
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
